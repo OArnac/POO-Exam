c3c1d94eacafa5ae5777aac7b2dccc4099f9c3b3
Olivier <olivier.arnac@laposte.net>
2020-05-25 16:00:04 +0200

    Y a rien qui marche alors voilà

--- config/routes.php
+++ config/routes.php
@@ -5,10 +5,9 @@ use Bramus\Router\Router;
 $router = new Router();
 $router->setNamespace('App\Controller');
 
-/**
- * Insérez vos routes ici
- */
-// $router->get('/file', 'class@all/list/index...');
+$router->get('/', 'AppController@index');
+
+$router->get('/vehicule', 'VehiculeController@index');
 
 $router->run();
 
